6528d8deb8b495a3972df8c58717efc65779d564
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-11-30 10:50:50 +0200

    Add listProductsFunction and productServiceApi to ServerlessApiStack

--- lib/serverless-api-stack.ts
+++ lib/serverless-api-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class ServerlessApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const listProductsFunction = new lambdaNodejs.NodejsFunction(
+      this,
+      "ListProductsFunction",
+      {
+        entry: "functions/list-products.ts",
+        handler: "handler",
+        runtime: lambda.Runtime.NODEJS_18_X,
+      },
+    )
+    
+    listProductsFunction.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['secretsmanager:GetSecretValue'],
+      resources: ['*'],
+    }))
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const productServiceApi = new apigateway.RestApi(this, "Endpoint", {
+      restApiName: "Product Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const products = productServiceApi.root.addResource("products");
+
+    products.addMethod("GET", new apigateway.LambdaIntegration(listProductsFunction));
+
   }
 }
